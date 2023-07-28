7827360954c1cbd814546faafbedbe086ad2ef8a
adityamahendrap <ptadityamahendrap@gmail.com>
2023-07-28 20:16:30 +0800

    fix assets path

--- src/stores/notes.js
+++ src/stores/notes.js
@@ -46,17 +46,17 @@ export const useNotesStore = defineStore("notes", () => {
       {
         name: "Favorites",
         isActive: false,
-        icon: "/src/assets/images/favorites-icon.svg",
+        icon: "@/assets/images/favorites-icon.svg",
       },
       {
         name: "Trash",
         isActive: false,
-        icon: "/src/assets/images/trash-icon.svg",
+        icon: "@/assets/images/trash-icon.svg",
       },
       {
         name: "Archived Notes",
         isActive: false,
-        icon: "/src/assets/images/archived-notes-icon.svg",
+        icon: "@/assets/images/archived-notes-icon.svg",
       },
     ],
   });
