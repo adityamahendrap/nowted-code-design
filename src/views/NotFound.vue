0ed412c0d3c5f974e2820169cdc34ffc57142195
adityamahendrap <ptadityamahendrap@gmail.com>
2023-07-28 12:30:17 +0800

    fix not found handler

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -8,13 +8,13 @@ const toMainPage = () => {
 </script>
 
 <template>
-  <main class="w-full py-12 bg-[#181818]">
+  <main class="w-full h-screen py-12 bg-[#181818]">
     <div class="text-center">
       <h1 class="font-semibold">Oof, looks like you got lost</h1>
-      <hr class="mx-auto mt-2 max-w-md" />
+      <hr class="mx-auto mt-2 max-w-md border-white/50" />
       <button
         @click="toMainPage"
-        class="mt-2 rounded-md bg-black/10 px-3 py-1 hover:opacity-80"
+        class="mt-2 rounded-md bg-[#242424] hover:bg-white/10 px-3 pt-1 pb-2"
       >
         Go to main page
       </button>
